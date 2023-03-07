f8e584e4b94ae341a2acd049313fb37bb3031a24
Sristi <sristi0108@gmail.com>
2023-03-07 12:19:31 +0530

    Format code

--- run.py
+++ run.py
@@ -1,93 +1,69 @@
-from flask import Flask,request, jsonify
+from flask import Flask, request, jsonify
 import requests
 from bs4 import BeautifulSoup
-app= Flask(__name__)
 
-@app.route("/cosmetics",methods=['GET','POST'])
+app = Flask(__name__)
+
+
+@app.route("/cosmetics", methods=["GET", "POST"])
 def home():
-    text=0
-    total=0
-    if request.method == 'POST':
-        # text = str(request.args["product_name"])
-        text= request.form.get('product_name')
-        if text is None:
-            return "product name not found …"
-
-
-# cosmetics
-
+    if request.method == "POST":
+        product = request.form.get("product_name")
+        if product is None:
+            return "product name not found"
+        # cosmetics
         url = "https://incidecoder.com/"
-        
+        res = requests.get(url + "search?query={}".format(product))
+        soup_data = BeautifulSoup(res.text, "html.parser")
 
-        res = requests.get(url+'search?query={}'.format(text))
-        soup_data = BeautifulSoup(res.text, 'html.parser')
-
-        links = soup_data.find_all('a', class_="klavika simpletextlistitem")
+        links = soup_data.find_all("a", class_="klavika simpletextlistitem")
         specific = links[0]["href"]
 
         details = requests.get(url + specific[1:])
-        soup = BeautifulSoup(details.text, 'html.parser')
-   
-        # ingredients = soup.find_all('a', class_="ingred-link black")
-        all_ing=set()
-        for li in soup.find_all('a', class_="ingred-link black"):
+        soup = BeautifulSoup(details.text, "html.parser")
+        all_ing = set()
+        for li in soup.find_all("a", class_="ingred-link black"):
             all_ing.add(li.text)
         print(all_ing)
-        # print(ingredients)
-
-        total=30
-        return jsonify({"product":text,"prediction": total})
-    
+        return jsonify({"product": product, "prediction": total})
 
 
-@app.route("/food",methods=['GET','POST'])
+
+@app.route("/food", methods=["GET", "POST"])
 def data():
-    text=0
-    total=0
-    if request.method == 'POST':
-        # text = str(request.args["product_name"])
-        text= request.form.get('product_name')
-        if text is None:
-            return "product name not found …"
-# food
-        url="https://in.openfoodfacts.org/"
-        x=text.split()
-        after_join='+'.join(x)
-        # print(after_join)
+    if request.method == "POST":
+        product = request.form.get("product_name")
+        if product is None:
+            return "product name not found"
+        # food
+        url = "https://in.openfoodfacts.org/"
+        x = product.split()
+        after_join = "+".join(x)
+        url2 = (
+            url
+            + "cgi/search.pl?search_terms={}".format(after_join)
+            + "&search_simple=1&action=process"
+        )
+        r = requests.get(url2)
+        htmlContent = r.text
+        soup = BeautifulSoup(htmlContent, "html.parser")
 
-
-
-        url2=url+"cgi/search.pl?search_terms={}".format(after_join)+"&search_simple=1&action=process"
-        # print(url2)
-        r=requests.get(url2)
-        htmlContent=r.text
-        soup=BeautifulSoup(htmlContent,'html.parser')
-
-        links = soup.find('ul', class_="products")
-        ingredients=None
-        i=0
-        while ingredients==None :
-            link= links.find_all("li")[i]
-            i+=1
-            anchors=link.find('a')
-        
+        links = soup.find("ul", class_="products")
+        ingredients = None
+        while ingredients == None:
+            link = links.find_all("li")[i]
+            anchors = link.find("a")
             specific = anchors["href"]
-        
-            # print(url + specific[1:])
             details = requests.get(url + specific[1:])
-            soup = BeautifulSoup(details.text, 'html.parser')
-        
-            ingredients = soup.find('ol', id="ordered_ingredients_list")
-            # print(ingredients)
+            soup = BeautifulSoup(details.text, "html.parser")
+            ingredients = soup.find("ol", id="ordered_ingredients_list")
 
-        all_ing=set()
+        all_ing = set()
         for li in ingredients.find_all("span"):
             all_ing.add(li.text)
-        print(all_ing)
-        total=30
-        return jsonify({"product":text,"prediction": total})
 
-if __name__ == '__main__':
+        return jsonify({"product": product, "prediction": total})
+
+
+if __name__ == "__main__":
     app.run(debug=True)
- 
-
